e94f32e6a9a2da50b26ab5b1da96c24e81b834ba
Rony Sinai <rony.sinai@gmail.com>
2024-03-30 13:40:20 +0300

    adding modal

---
+++ my-form-app/components/NavBar.jsx
@@ -0,0 +1,54 @@
+"use client";
+import Link from "next/link";
+import styled from "styled-components";
+
+export default function NavBar({ setOpenModal }) {
+  return (
+    <>
+      <Container>
+        <Logo>LOGO</Logo>
+        <Search>
+          <Input placeholder="Search"></Input>
+          <button onClick={() => setOpenModal(true)}>Ask Question!</button>
+        </Search>
+        <button>Logout</button>
+      </Container>
+    </>
+  );
+}
+const Logo = styled.div`
+  background-color: red;
+  height: 50px;
+  width: 10%;
+  color: aliceblue;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Container = styled.div`
+  background-color: white;
+  height: 50px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+`;
+
+const Search = styled.div`
+  width: 70%;
+  display: flex;
+  align-items: center;
+`;
+
+const Input = styled.input`
+  width: 70%;
+  height: 100%;
+  box-sizing: border-box;
+`;
+
+const Button = styled.button`
+  height: 32px;
+  box-sizing: border-box;
+`;
---
+++ my-form-app/components/UserQuestionModal.jsx
@@ -0,0 +1,60 @@
+"use client";
+import React from "react";
+import styled from "styled-components";
+
+export default function UserQuestionsModal({ open, onClose }) {
+  if (!open) {
+    return null;
+  }
+  return (
+    <PageWrapper>
+      <ModalContainer>
+        <h2>Ask Question</h2>
+        <div>Title</div>
+        <input></input>
+        <div> Topic Description</div>
+        <textarea></textarea>
+        <div>Tags separated by,</div>
+        <input></input>
+        <Submit>Submit</Submit>
+        <CloseBtn onClick={onClose}>X</CloseBtn>
+      </ModalContainer>
+    </PageWrapper>
+  );
+}
+
+const PageWrapper = styled.div`
+  background-color: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  width: 100%;
+  height: 100%;
+`;
+
+const ModalContainer = styled.div`
+  gap: 10px;
+  max-width: 600px;
+  width: 100%;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  background-color: white;
+  box-shadow: 0px 0px 18px 0px rgba(0, 0, 0, 0.75);
+  height: 60vh;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const CloseBtn = styled.p`
+  position: fixed;
+  top: 9px;
+  right: 9px;
+  border: 1px solid black;
+`;
+
+const Submit = styled.button`
+  background-color: gray;
+  height: 30px;
+  width: fit-content;
+`;
